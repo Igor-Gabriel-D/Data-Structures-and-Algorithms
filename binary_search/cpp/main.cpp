539842627e61850a833316536cc954620b0ddb31
Igor-Gabriel-D <igordantas.dev@gmail.com>
2025-12-18 22:10:53 -0300

    feat: binary search function (cpp)

--- binary_search/cpp/main.cpp
+++ binary_search/cpp/main.cpp
@@ -14,6 +14,43 @@ void gen_orded_list(int arr[], size_t n, int min){
 }
 
 
+int binary_search(int arr[], size_t n , int value){
+    int res;
+    int min = 0;
+    int max = n - 1;
+    int mid = (min + max) / 2;
+
+    while( min != max){
+        	//else if(arr[mid+1] == value) return mid+1;
+	//std::cout << mid << std::endl;
+
+	if(arr[mid] < value){
+	    min = mid + 1;
+	    mid = ((max - min) / 2) + min;
+	} else if( arr[mid] > value){
+	    max = mid - 1;
+	    mid = ((max - min) / 2) + min;
+	}
+
+        if(arr[mid] == value) return mid;
+
+
+
+	
+
+    }
+
+     
+    return 0;
+}
+
+int linear_search(int arr[], size_t n, int value){
+    for(int i = 0; i < n; i++){
+    	if(arr[i] == value) return i;
+    }
+    return -1;
+}
+
 int main(){
     std::cout << "Binary Search" << std::endl;
 
@@ -26,6 +63,14 @@ int main(){
     for(int i = 0; i < 100; i++) std::cout << arr[i] << " ";
 
     std::cout << std::endl;
+
+    int value = arr[(rand() % 100)];
+    std::cout << value << std::endl;
+
+    std::cout << std::endl;
+    std::cout << linear_search(arr, 100, value) << std::endl;
+    std::cout << binary_search(arr, 100, value) << std::endl;
+
 
     return 0;
 }
